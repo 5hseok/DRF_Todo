7aad9683838404744b4433a97a0ce59d8b07e458
5hseok <ohhs1010@gmail.com>
2024-01-04 01:38:03 +0900

    allowed host 설정

--- mytodo/settings.py
+++ mytodo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7l$zb(^%d6c1im9!d700cg$g5rbdz6pylh!vv^803odmvpo#rv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
