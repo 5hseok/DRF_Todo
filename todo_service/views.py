e9c93c418745a0d1ff282d797cabbd111f01bf33
5hseok <ohhs1010@gmail.com>
2024-01-13 15:10:41 +0900

    Feat : 상세 조회 API 상세 조회 API 뷰 추가

--- todo_service/views.py
+++ todo_service/views.py
@@ -1,16 +1,19 @@
-from rest_framework import generics
+from rest_framework.generics import get_object_or_404
 from rest_framework import viewsets
-from .serializers import TodoSimpleSerializer
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from .models import Todo
-
+from .serializers import TodoSimpleSerializer, TodoDetailSerializer
 class TodosAPIView(APIView):
     def get(self, request):
         todos = Todo.objects.filter(complete = False)      # complete가 False인 Todo들은 필터링 
         serializer = TodoSimpleSerializer(todos, many=True)
         return Response(serializer.data, status = status.HTTP_200_OK)
 
-
-# Create your views here.
+class TodoAPIView(APIView):
+    def get(self, request, pk):
+        todo = get_object_or_404(Todo, pk=pk)
+        serializer = TodoDetailSerializer(todo)
+        return Response(serializer.data, status = status.HTTP_200_OK)
+        
