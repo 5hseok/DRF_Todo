1aab393baee91f661fc191df9f90f61ec8aa3f1b
5hseok <ohhs1010@gmail.com>
2024-01-13 17:12:43 +0900

    ♻️ Refactor : model 속성 변경 created(todo 생성 시각)과 completed(todo 완료 시각)의 속성명에 _time을 추가하였습니다.

--- todo_service/models.py
+++ todo_service/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class Todo(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    created = models.DateTimeField(auto_now_add=True)
+    created_time = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False)
-    completed = models.DateTimeField(auto_now=True)
-    important = models.BooleanField(default=False)
+    completed_time = models.DateTimeField(auto_now=True)
+    important = models.BooleanField(default=False)  
     
     def __str__(self):
         return self.title
