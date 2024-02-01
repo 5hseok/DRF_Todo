2c8409e3a654be0e596bf09e8b79127f5ebe2b60
5hseok <ohhs1010@gmail.com>
2024-02-01 19:16:23 +0900

    Add environment variable support for SECRET_KEY

--- mytodo/settings.py
+++ mytodo/settings.py
@@ -11,16 +11,27 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import environ, ImproperlyConfigured, os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env(DEBUG=(bool, True))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
+# 함수 구현
+def get_env_variable(var_name):
+    try:
+        return env(var_name)
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(var_name)
+        raise ImproperlyConfigured(error_msg)
+
+# SECRET_KEY 설정
+SECRET_KEY = get_env_variable('SECRET_KEY')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7l$zb(^%d6c1im9!d700cg$g5rbdz6pylh!vv^803odmvpo#rv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
